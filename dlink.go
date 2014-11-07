fece1863f342e589d937b49058a10b665fc120ab
Scusi <scusi@snurn.de>
2014-11-08 00:48:12 +0100

    made dlinkWeb.go working

--- dlink.go
+++ dlink.go
@@ -1,5 +1,5 @@
-// dlink opens any number of files and replaces 'http://' to 'hxxp://', in order to make links non clickable anymore.
-// open any number of given files 
+// dlink.go - opens any number of files and replaces 'http://' to 'hxxp://', in order to make links non clickable anymore.
+// open any number of given files
 // replace all links into non clickable versions within the content of each file
 // write the file back to it's original filename
 //
@@ -8,62 +8,70 @@
 
 package main
 
-import(
-    "os"
-    "io/ioutil"
-    "fmt"
-    "regexp"
+import (
+	"os"
+	"fmt"
+    "flag"
+	"regexp"
+	"io/ioutil"
 )
+
+var cmd string
+
+func init(){
+    flag.StringVar(&cmd, "cmd", "delink", "command, can be 'delink' and 'relink'. 'delink' is default")
+}
 
 // checkErr a function to check the error return value of another function.
 func checkErr(err error) {
-    if err != nil {
-        fmt.Println("Error: ", err)
-        os.Exit(2)
-    }
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(2)
+	}
 }
 
 // loader - returns the content of a given file as []byte
 // takes a string (the filename of the file to read) as argument
 // returns a []byte (containing content of file) and error
 func loader(filename string) (content []byte, err error) {
-    content, err = ioutil.ReadFile(filename)
-    if err != nil {
-         return nil, err
-    }
-    return content, nil
+	content, err = ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return content, nil
 }
 
-// writeFile takes an array of bytes, and a filename as a string as argument, writes 
+// writeFile takes an array of bytes, and a filename as a string as argument, writes
 // those bytes into a file with the supplied name (filename), returns the number of bytes written
 func writeFile(b []byte, filename string) (i int) {
-    f, err := os.Create(filename)
-    checkErr(err)
-    defer f.Close()
-    i, err = f.Write(b)
-    checkErr(err)
-    return i
+	f, err := os.Create(filename)
+	checkErr(err)
+	defer f.Close()
+	i, err = f.Write(b)
+	checkErr(err)
+	return i
 }
 
 // main takes arbitary filenames as arguments from command line and rewrites those files.
 func main() {
-    // compile the regex, make sure it is valid and does actually compile
-    re := regexp.MustCompile(`http(s)?:\/\/`) // matches 'http://' as well as 'https://'
-    // get the arguments from the command line
-    args := os.Args[1:]
-    // iterate over arguments
-    for i := 0; i<len(args); i++ {
-        // get current filename from arguments
-        filename := args[i]
-	// load file content and check for errors
-        content, err := loader(filename)
-        checkErr(err)
-	// replace what matches our regex
-	//  replaces 'http://'  into 'hXXp://' or subsequent 'https://' into 'hXXps://'
-        new_content := re.ReplaceAll(content, []byte("hXXp${1}://")) 
-	// write content (with replacements) back to file
-	i := writeFile(new_content, filename)
-	// print out how many bytes we wrote onto what file
-	fmt.Printf("%d bytes written to '%s'\n", i, filename)
-    }
+    flag.Parse()
+	// compile the regex, make sure it is valid and does actually compile
+	re := regexp.MustCompile(`http(s)?:\/\/`) // matches 'http://' as well as 'https://'
+	// get the arguments from the command line
+	args := os.Args[1:]
+	// iterate over arguments
+	for i := 0; i < len(args); i++ {
+	    // get current filename from arguments
+	    filename := args[i]
+	    // load file content and check for errors
+	    content, err := loader(filename)
+	    checkErr(err)
+	    // replace what matches our regex
+	    //  replaces 'http://'  into 'hXXp://' or subsequent 'https://' into 'hXXps://'
+	    new_content := re.ReplaceAll(content, []byte("hXXp${1}://"))
+	    // write content (with replacements) back to file
+	    i := writeFile(new_content, filename+".dlinked")
+	    // print out how many bytes we wrote onto what file
+	    fmt.Printf("%d bytes written to '%s'\n", i, filename+".dlinked")
+	}
 }
--- dlinkWeb.go
+++ dlinkWeb.go
@@ -1,4 +1,4 @@
-// dlink as a webservice
+// dlinkWeb.go - dlink as a webservice
 //
 // 24.01.2014 - ICE 1090
 //
@@ -8,24 +8,91 @@
 package main
 
 import(
-    "io/ioutil"
+    "log"
+    "fmt"
+	"regexp"
     "net/http"
+    "io/ioutil"
+    "unicode/utf8"
     "html/template"
-    "regexp"
-    "errors"
+    "net/http/httputil"
 )
 
 // constants and variables:
-var templates = template.Must(template.ParseFiles("tmpl/upload.html", "tmpl/download.html"))
+var templates = template.Must(
+    template.ParseFiles(
+        "tmpl/upload.html", 
+        "tmpl/download.html",
+    ),
+)
+
+type File struct {
+    Name        string
+    Size        int
+    ContentType string
+    Content     []byte
+    Error       string
+}
 
 func upHandler(w http.ResponseWriter, r *http.Request) {
     t, _ := template.ParseFiles("tmpl/upload.html")
-    p :=  ""
+    p := ""
     t.Execute(w, p)
 }
 
+func doHandler(w http.ResponseWriter, r *http.Request) {
+    // get multipart-file from request
+    mpr, mpHeader, err := r.FormFile("file")
+    //mpr, _, err := r.FormFile("file")
+    if err != nil {
+        log.Fatal(err)
+    }
+    content, err := ioutil.ReadAll(mpr)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+    //Input Validation
+    // - Input should not be over a specified size
+    if len(content) >= 1024 * 1024 {
+        http.Error(w, "Input to large", http.StatusInternalServerError)
+        return
+    }
+    // - Input should just contain runes (valid Unicode Codepoints)
+    if utf8.Valid(content) == false {
+        //http.Error(w, "Input contains non unicode characters", http.StatusInternalServerError)
+        t, _ := template.ParseFiles("tmpl/upload.html")
+        p := File{}
+        p.Error = "Input contains non unicode characters"
+        t.Execute(w, p)
+        return
+    }
+    // replace content
+	re := regexp.MustCompile(`http(s)?:\/\/`) // matches 'http://' as well as 'https://'
+	content = re.ReplaceAll(content, []byte("hXXp${1}://"))
+    //fmt.Fprintf(w, "%s", content)
+    //t, _ := template.ParseFiles("tmpl/download.html")
+    t, _ := template.ParseFiles("tmpl/upload.html")
+    p := File{}
+    p.Content = content
+    p.Size = len(content)
+    p.ContentType = mpHeader.Header.Get("Content-Type")
+    p.Name = mpHeader.Filename
+    t.Execute(w, p)
+}
+
+func reqDumper(w http.ResponseWriter, r *http.Request) {
+    dumpedReq, err := httputil.DumpRequest(r, true)
+    if err != nil {
+        log.Fatal("httputil.DumpRequestOut:", err)
+    }
+    log.Printf("%s\n", dumpedReq)
+    fmt.Fprintf(w, "%s", dumpedReq)
+}
+
 func main() {
+    http.HandleFunc("/", upHandler)
     http.HandleFunc("/up/", upHandler)
-    //http.HandleFunc("/do/", doHandler)
-    http.ListenAndServer(":9090", nil)
+    http.HandleFunc("/do/", doHandler)
+    http.HandleFunc("/dump/", reqDumper)
+    http.ListenAndServe(":9090", nil)
 }
